43a2a377d4070c0716ff419ae70506d2a51b88af
Mark McCarthy <mark.joseph.mccarthy@gmail.com>
2023-09-11 14:43:34 +0100

    db connection added

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 // This imports the express library
-
 const app = express();
 // This renames express to app as per conventional useage
-
 const port = 5001;
 // allows us to decide what port number to assign
 
-app.use("/public", express.static("public"))
+
+
+// app.use("/public", express.static("public"))
 // express.static("public") setups a static webserver pulling files from the public folder
 // app.use creates a route so that when we type in localhost:5001/public it follows this route
 
