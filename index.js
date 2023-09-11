c95dd357d702c1605914785e5d70cd88d591738b
Mark McCarthy <mark.joseph.mccarthy@gmail.com>
2023-09-11 11:13:38 +0100

    annotations added

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+// This imports the express library
+
 const app = express();
+// This renames express to app as per conventional useage
+
 const port = 5001;
+// allows us to decide what port number to assign
 
 app.use("/public", express.static("public"))
+// express.static("public") setups a static webserver pulling files from the public folder
+// app.use creates a route so that when we type in localhost:5001/public it follows this route
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
+// This is the listener function which only listens into http request on port 5001, the anonomous 
+// function runs just once when the listener is setup
